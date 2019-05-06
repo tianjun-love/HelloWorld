838275e8a8510ae6cd8c98cb103ce349b80c2d8a
tianjun-love <tianjun_love@live.com>
2019-05-06 21:20:21 +0800

    hello world

---
+++ test/src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello world !" << endl;
+
+	//暂停
+	system("pause");
+
+	return 0;
+}
